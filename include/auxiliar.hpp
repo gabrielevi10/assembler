4f0e8c7b18d70e6a6c7933f1b2deebeacc60abbb
gabrielevi10 <gabrielevigomes@gmail.com>
2018-10-19 10:06:24 -0300

    Adicionado arquivo auxiliar

---
+++ include/auxiliar.hpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+const vector<string> split(const string &s, const char &c);
+
+string remove_initial_spaces(string in);
+
+string remove_final_spaces(string in);
+
+string remove_unecessary_spaces(string in);
+
+string format_line(string line);
